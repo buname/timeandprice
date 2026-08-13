b278b3404819e7c23279f53ef76ff1659bce3e42
buname <yahyayildiran10@hotmail.com>
2026-08-13 10:05:27 +0300

    Open Google sign-in from the landing log in and sign up buttons.

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ window.TP_CONFIG = {
   //   http://127.0.0.1:8000
   //   http://localhost:8000
   // Later add your Vercel URL the same way.
-  googleClientId: '70341597246-79thd71mqi1t80jgit0b2b6m3ihrpo9j.apps.googleusercontent.com'
+  googleClientId: '70341597246-79thd71mqi1t80jgit0b2b6m3ihrpo9j.apps.googleusercontent.com',
+  appStorageKey: 'timeandprice.app.v5'
 };
